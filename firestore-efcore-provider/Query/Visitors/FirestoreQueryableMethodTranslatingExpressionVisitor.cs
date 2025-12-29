e103f43cd777200730e9812c6bd90cc8da709621
Pedro Hurtado <pedrohurt@gmail.com>
2025-12-29 14:15:11 +0100

    refactor: reorganize TranslateMethod regions by implementation status

--- firestore-efcore-provider/Query/Visitors/FirestoreQueryableMethodTranslatingExpressionVisitor.cs
+++ firestore-efcore-provider/Query/Visitors/FirestoreQueryableMethodTranslatingExpressionVisitor.cs
@@ -115,67 +115,11 @@ namespace Firestore.EntityFrameworkCore.Query.Visitors
 
         #region Translate Methods
 
-        protected override ShapedQueryExpression? TranslateFirstOrDefault(
-            ShapedQueryExpression source,
-            LambdaExpression? predicate,
-            Type returnType,
-            bool returnDefault)
-            => FirestoreQueryExpression.TranslateFirstOrDefault(new(source, predicate, returnType, returnDefault));
-
-        protected override ShapedQueryExpression TranslateSelect(
-            ShapedQueryExpression source,
-            LambdaExpression selector)
-        {
-            // Include expressions - delegate to MicroDomain (one-liner)
-            if (selector.Body is IncludeExpression includeExpression)
-                return FirestoreQueryExpression.TranslateInclude(new(source, includeExpression));
-
-            // Identity projection (x => x)
-            if (selector.Body == selector.Parameters[0])
-                return source;
-
-            // Type conversion projection
-            if (selector.Body is UnaryExpression unary &&
-                unary.NodeType == ExpressionType.Convert &&
-                unary.Operand == selector.Parameters[0])
-                return source;
-
-            // TODO: Real projections will be implemented in Phase 3 with FirestoreProjectionTranslator
-            return source;
-        }
-
-        protected override ShapedQueryExpression? TranslateWhere(
-            ShapedQueryExpression source,
-            LambdaExpression predicate)
-        {
-            // Replace runtime parameters before delegating to the Slice
-            var parameterReplacer = new RuntimeParameterReplacer(QueryCompilationContext);
-            var evaluatedBody = parameterReplacer.Visit(predicate.Body);
-
-            return FirestoreQueryExpression.TranslateWhere(new(source, evaluatedBody));
-        }
-
-        #endregion
-
-        #region Not Implemented Methods
-
-        protected override ShapedQueryExpression? TranslateAll(ShapedQueryExpression source, LambdaExpression predicate)
-            => throw new NotImplementedException();
-
         protected override ShapedQueryExpression? TranslateAny(ShapedQueryExpression source, LambdaExpression? predicate)
             => FirestoreQueryExpression.TranslateAny(new(source, predicate));
 
         protected override ShapedQueryExpression? TranslateAverage(ShapedQueryExpression source, LambdaExpression? selector, Type resultType)
             => FirestoreQueryExpression.TranslateAverage(new(source, selector, resultType));
-
-        protected override ShapedQueryExpression? TranslateCast(ShapedQueryExpression source, Type castType)
-            => throw new NotImplementedException();
-
-        protected override ShapedQueryExpression? TranslateConcat(ShapedQueryExpression source1, ShapedQueryExpression source2)
-            => throw new NotImplementedException();
-
-        protected override ShapedQueryExpression? TranslateContains(ShapedQueryExpression source, Expression item)
-            => throw new NotImplementedException();
 
         protected override ShapedQueryExpression? TranslateCount(ShapedQueryExpression source, LambdaExpression? predicate)
             => FirestoreQueryExpression.TranslateCount(new(source, predicate));
@@ -183,29 +127,12 @@ namespace Firestore.EntityFrameworkCore.Query.Visitors
         protected override ShapedQueryExpression? TranslateDefaultIfEmpty(ShapedQueryExpression source, Expression? defaultValue)
             => FirestoreQueryExpression.TranslateDefaultIfEmpty(new(source, defaultValue));
 
-        protected override ShapedQueryExpression? TranslateDistinct(ShapedQueryExpression source)
-            => throw new NotImplementedException();
-
-        protected override ShapedQueryExpression? TranslateElementAtOrDefault(ShapedQueryExpression source, Expression index, bool returnDefault)
-            => throw new NotImplementedException();
-
-        protected override ShapedQueryExpression? TranslateExcept(ShapedQueryExpression source1, ShapedQueryExpression source2)
-            => throw new NotImplementedException();
-
-        protected override ShapedQueryExpression? TranslateGroupBy(ShapedQueryExpression source, LambdaExpression keySelector, LambdaExpression? elementSelector, LambdaExpression? resultSelector)
-            => throw new NotImplementedException();
-
-        protected override ShapedQueryExpression? TranslateGroupJoin(ShapedQueryExpression outer, ShapedQueryExpression inner, LambdaExpression outerKeySelector, LambdaExpression innerKeySelector, LambdaExpression resultSelector)
-            => throw new NotImplementedException();
-
-        protected override ShapedQueryExpression? TranslateIntersect(ShapedQueryExpression source1, ShapedQueryExpression source2)
-            => throw new NotImplementedException();
-
-        protected override ShapedQueryExpression? TranslateJoin(ShapedQueryExpression outer, ShapedQueryExpression inner, LambdaExpression outerKeySelector, LambdaExpression innerKeySelector, LambdaExpression resultSelector)
-            => throw new NotImplementedException();
-
-        protected override ShapedQueryExpression? TranslateLastOrDefault(ShapedQueryExpression source, LambdaExpression? predicate, Type returnType, bool returnDefault)
-            => throw new NotImplementedException();
+        protected override ShapedQueryExpression? TranslateFirstOrDefault(
+            ShapedQueryExpression source,
+            LambdaExpression? predicate,
+            Type returnType,
+            bool returnDefault)
+            => FirestoreQueryExpression.TranslateFirstOrDefault(new(source, predicate, returnType, returnDefault));
 
         protected override ShapedQueryExpression? TranslateLeftJoin(
             ShapedQueryExpression outer,
@@ -260,38 +187,42 @@ namespace Firestore.EntityFrameworkCore.Query.Visitors
                 $"Use .Reference() to configure DocumentReference navigations.");
         }
 
-        protected override ShapedQueryExpression? TranslateLongCount(ShapedQueryExpression source, LambdaExpression? predicate)
-            => throw new NotImplementedException();
-
         protected override ShapedQueryExpression? TranslateMax(ShapedQueryExpression source, LambdaExpression? selector, Type resultType)
             => FirestoreQueryExpression.TranslateMax(new(source, selector, resultType));
 
         protected override ShapedQueryExpression? TranslateMin(ShapedQueryExpression source, LambdaExpression? selector, Type resultType)
             => FirestoreQueryExpression.TranslateMin(new(source, selector, resultType));
 
-        protected override ShapedQueryExpression? TranslateOfType(ShapedQueryExpression source, Type resultType)
-            => throw new NotImplementedException();
-
         protected override ShapedQueryExpression? TranslateOrderBy(ShapedQueryExpression source, LambdaExpression keySelector, bool ascending)
             => FirestoreQueryExpression.TranslateOrderBy(new(source, keySelector, ascending, IsFirst: true));
 
-        protected override ShapedQueryExpression? TranslateReverse(ShapedQueryExpression source)
-            => throw new NotImplementedException();
+        protected override ShapedQueryExpression TranslateSelect(
+            ShapedQueryExpression source,
+            LambdaExpression selector)
+        {
+            // Include expressions - delegate to MicroDomain (one-liner)
+            if (selector.Body is IncludeExpression includeExpression)
+                return FirestoreQueryExpression.TranslateInclude(new(source, includeExpression));
 
-        protected override ShapedQueryExpression? TranslateSelectMany(ShapedQueryExpression source, LambdaExpression collectionSelector, LambdaExpression resultSelector)
-            => throw new NotImplementedException();
+            // Identity projection (x => x)
+            if (selector.Body == selector.Parameters[0])
+                return source;
 
-        protected override ShapedQueryExpression? TranslateSelectMany(ShapedQueryExpression source, LambdaExpression selector)
-            => throw new NotImplementedException();
+            // Type conversion projection
+            if (selector.Body is UnaryExpression unary &&
+                unary.NodeType == ExpressionType.Convert &&
+                unary.Operand == selector.Parameters[0])
+                return source;
+
+            // TODO: Real projections will be implemented in Phase 3 with FirestoreProjectionTranslator
+            return source;
+        }
 
         protected override ShapedQueryExpression? TranslateSingleOrDefault(ShapedQueryExpression source, LambdaExpression? predicate, Type returnType, bool returnDefault)
             => FirestoreQueryExpression.TranslateSingleOrDefault(new(source, predicate, returnType, returnDefault));
 
         protected override ShapedQueryExpression? TranslateSkip(ShapedQueryExpression source, Expression count)
             => FirestoreQueryExpression.TranslateSkip(new(source, count));
-
-        protected override ShapedQueryExpression? TranslateSkipWhile(ShapedQueryExpression source, LambdaExpression predicate)
-            => throw new NotImplementedException();
 
         protected override ShapedQueryExpression? TranslateSum(ShapedQueryExpression source, LambdaExpression? selector, Type resultType)
             => FirestoreQueryExpression.TranslateSum(new(source, selector, resultType));
@@ -306,11 +237,80 @@ namespace Firestore.EntityFrameworkCore.Query.Visitors
         private ShapedQueryExpression TranslateTakeLast(ShapedQueryExpression source, Expression count)
             => FirestoreQueryExpression.TranslateLimit(new(source, count, IsLimitToLast: true));
 
-        protected override ShapedQueryExpression? TranslateTakeWhile(ShapedQueryExpression source, LambdaExpression predicate)
-            => throw new NotImplementedException();
-
         protected override ShapedQueryExpression? TranslateThenBy(ShapedQueryExpression source, LambdaExpression keySelector, bool ascending)
             => FirestoreQueryExpression.TranslateOrderBy(new(source, keySelector, ascending, IsFirst: false));
+
+        protected override ShapedQueryExpression? TranslateWhere(
+            ShapedQueryExpression source,
+            LambdaExpression predicate)
+        {
+            // Replace runtime parameters before delegating to the Slice
+            var parameterReplacer = new RuntimeParameterReplacer(QueryCompilationContext);
+            var evaluatedBody = parameterReplacer.Visit(predicate.Body);
+
+            return FirestoreQueryExpression.TranslateWhere(new(source, evaluatedBody));
+        }
+
+        #endregion
+
+        #region Not Implemented Methods
+
+        protected override ShapedQueryExpression? TranslateAll(ShapedQueryExpression source, LambdaExpression predicate)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateCast(ShapedQueryExpression source, Type castType)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateConcat(ShapedQueryExpression source1, ShapedQueryExpression source2)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateContains(ShapedQueryExpression source, Expression item)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateDistinct(ShapedQueryExpression source)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateElementAtOrDefault(ShapedQueryExpression source, Expression index, bool returnDefault)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateExcept(ShapedQueryExpression source1, ShapedQueryExpression source2)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateGroupBy(ShapedQueryExpression source, LambdaExpression keySelector, LambdaExpression? elementSelector, LambdaExpression? resultSelector)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateGroupJoin(ShapedQueryExpression outer, ShapedQueryExpression inner, LambdaExpression outerKeySelector, LambdaExpression innerKeySelector, LambdaExpression resultSelector)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateIntersect(ShapedQueryExpression source1, ShapedQueryExpression source2)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateJoin(ShapedQueryExpression outer, ShapedQueryExpression inner, LambdaExpression outerKeySelector, LambdaExpression innerKeySelector, LambdaExpression resultSelector)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateLastOrDefault(ShapedQueryExpression source, LambdaExpression? predicate, Type returnType, bool returnDefault)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateLongCount(ShapedQueryExpression source, LambdaExpression? predicate)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateOfType(ShapedQueryExpression source, Type resultType)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateReverse(ShapedQueryExpression source)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateSelectMany(ShapedQueryExpression source, LambdaExpression collectionSelector, LambdaExpression resultSelector)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateSelectMany(ShapedQueryExpression source, LambdaExpression selector)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateSkipWhile(ShapedQueryExpression source, LambdaExpression predicate)
+            => throw new NotImplementedException();
+
+        protected override ShapedQueryExpression? TranslateTakeWhile(ShapedQueryExpression source, LambdaExpression predicate)
+            => throw new NotImplementedException();
 
         protected override ShapedQueryExpression? TranslateUnion(ShapedQueryExpression source1, ShapedQueryExpression source2)
             => throw new NotImplementedException();
